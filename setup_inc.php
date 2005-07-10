3be24da77921b6cdc6fd148e1b16ebc361b1ecfc
Marc Archuleta <archuleta37@users.sourceforge.net>
2005-07-10 19:09:32 +0000

    change line 215 from file_exists('/proc/loadavg') to is_readable('/proc/loadavg') solves issue with install not working where loadavg permissions are limited

--- setup_inc.php
+++ setup_inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_kernel/setup_inc.php,v 1.6 2005/06/28 07:45:45 spiderr Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_kernel/setup_inc.php,v 1.7 2005/07/10 19:09:32 archuleta37 Exp $
  * @package kernel
  * @subpackage functions
  */
@@ -212,7 +212,7 @@ if( $gBitDb->isValid() ) {
 	// check to see if max server load threshold is enabled
 	$use_load_threshold = $gBitSystem->getPreference('use_load_threshold', 'n');
 	// get average server load in the last minute
-	if (file_exists('/proc/loadavg') && $load = file('/proc/loadavg'))
+	if (is_readable('/proc/loadavg') && $load = file('/proc/loadavg'))
 	{
 		list($server_load) = explode(' ', $load[0]);
 		$smarty->assign('server_load', $server_load);

1e34fd8f64e0615c68734ecf2cd735383ef00972
Lester Caine <lester@lsces.co.uk>
2005-08-17 08:51:15 +0000

    Add phpdoc notes

--- modules/mod_time.php
+++ modules/mod_time.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_kernel/modules/mod_time.php,v 1.1.2.2 2005/08/17 08:51:15 lsces Exp $
+ *
+ * Copyright ( c ) 2004 bitweaver.org
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ * @package kernel
+ * @subpackage modules
+ */
+
 if( !empty( $module_params ) ) {
 	$gBitSmarty->assign( 'modParams', $module_params );
 }

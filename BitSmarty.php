f0b0d204282d6e2e8486e537628a7fe48592d1e0
Max Kremmel <xing@synapse.plus.com>
2005-09-20 09:07:25 +0000

    close off <input> element

--- BitSmarty.php
+++ BitSmarty.php
@@ -3,7 +3,7 @@
  * Smarty Library Inteface Class
  *
  * @package Smarty
- * @version $Header: /cvsroot/bitweaver/_bit_kernel/BitSmarty.php,v 1.1.1.1.2.15 2005/09/04 18:09:45 squareing Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_kernel/BitSmarty.php,v 1.1.1.1.2.16 2005/09/20 09:07:25 squareing Exp $
  */
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
@@ -175,7 +175,7 @@ function add_link_ticket($tpl_source, &$smarty)
 		$to = '<form\\1><input type="hidden" name="tk" value="{$gBitUser->mTicket}" />';
  		$tpl_source = preg_replace( $from, $to, $tpl_source );
 		if( strpos( $tpl_source, '{form}' ) ) {
-			$tpl_source = str_replace( '{form}', '{form}<input type="hidden" name="tk" value="{$gBitUser->mTicket}">', $tpl_source );
+			$tpl_source = str_replace( '{form}', '{form}<input type="hidden" name="tk" value="{$gBitUser->mTicket}" />', $tpl_source );
 		} elseif( strpos( $tpl_source, '{form ' ) ) {
 			$from = '#\{form(\}| [^\}]*)\}#i';
 			$to = '{form\\1}<input type="hidden" name="tk" value="{$gBitUser->mTicket}" />';

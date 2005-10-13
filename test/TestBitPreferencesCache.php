7df77e4b3f7704177049b9fd2e83665d8714f20c
Jan Lindï¿½ker <docekal@users.sourceforge.net>
2005-10-13 19:53:19 +0000

    Removed inheritance from another tescase. The reason is that the testframework does not run the test correctly otherwise. The problem occurs if the test are executed in the wron order. It is possible to provoke this error by requiering the test file before running the testcase. Investigation why this happens will be underateken, since this problem is inconvenient.

--- test/TestBitPreferencesCache.php
+++ test/TestBitPreferencesCache.php
@@ -6,6 +6,8 @@ require_once(KERNEL_PKG_PATH.'test/TestBitPreferences.php');
 class TestBitPreferencesCache extends TestBitPreferences {
 
 
+    var $name = "TestBitPreferencesCache";
+
     function setUp ()
     {
 	global $gBitDb, $gCache;
@@ -17,10 +19,6 @@ class TestBitPreferencesCache extends TestBitPreferences {
 	$this->test->mDebug = false;
 	$gBitDb = $tmpDB;
 	$gCache = $tmpCache;
-	// $this->assertNotNull($this->test),
-	//                    'Error during initialisation');
-	// This check is probably not needed - it only adds to the passes
-	// and if it is not true it does not save anything anyway.
     }
 
     function tearDown ()
@@ -29,73 +27,44 @@ class TestBitPreferencesCache extends TestBitPreferences {
     }
 
 
-  /*
-
-  We are using inheritance instead
-    var $name = "TestBitPreferencesCache";
-
-    function initBitPreferences()
-    {
-	global $gBitDb;
-	$tmpDB = $gBitDb;
-	$gBitDb = NULL;
-        $test = new BitPreferences($this->name);
-	$gBitDb = $tmpDB;
-	return $test;
-    }
+# [tests]
 
     function TestBitPreferencesCache()
     {
-	#global $gCache;
-	#if (!is_object($gCache)) {
-	#  print "We have LANDED <br />\n";
-	#	$this = NULL;
-	#	return;
-	#}
-        $test = $this->initBitPreferences();
-	// This check is probably not needed - it only adds to the passes
-	// and if it is not true it does not save anything anyway.
-
-        // Assert::equals($test != NULL, 'Error during initialisation');
+      // no general initalization
 	
     }
     
     function testGetNonexistentItem()
     {
-        $test = $this->initBitPreferences();
-        $this->assertNull($test->getPreference("test"));
+        $this->assertNull($this->test->getPreference("test"));
     }
     
     function testSetNonexistentItem()
     {
-        $test = $this->initBitPreferences();
-        $test->setPreference("test", "123");
-        $this->assertEqual($test->getPreference("test"), "123");
+        $this->test->setPreference("test", "123");
+        $this->assertEqual($this->test->getPreference("test"), "123");
     }
     
     function testSetDefaultItem()
     {
-        $test = $this->initBitPreferences();
-        $test->setDefaultPreference("test", "456");
-        $this->assertEqual($test->getPreference("test"), "123");
+	$this->test->setPreference("test", "123");
+        $this->test->setDefaultPreference("test", "456");
+        $this->assertEqual($this->test->getPreference("test"), "123");
     }
     
     function testSetAsDefaultItem()
     {
-        $test = $this->initBitPreferences();
-        $test->setDefaultPreference("test", "456");
-        $test->setPreference("test", "456");
-        $this->assertEqual($test->getPreference("test"), "456");
+        $this->test->setDefaultPreference("test", "456");
+        $this->test->setPreference("test", "456");
+        $this->assertEqual($this->test->getPreference("test"), "456");
     }
 
     function testResetItem()
     {
-        $test = $this->initBitPreferences();
-        $test->setDefaultPreference("test", "456");
-        $test->setPreference("test", NULL);
-        $this->assertEqual($test->getPreference("test"), "456");
+        $this->test->setDefaultPreference("test", "456");
+        $this->test->setPreference("test", NULL);
+        $this->assertEqual($this->test->getPreference("test"), "456");
     }
-  */
-
 }
 ?>

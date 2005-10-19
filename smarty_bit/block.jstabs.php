aa1db2ed0c2a1cc500764e1f347b911dfa433d18
Max Kremmel <xing@synapse.plus.com>
2005-10-19 00:04:29 +0000

    allow for custom selection of active tab using {jstabs tab=n} where n is tab index number starting with 0

--- smarty_bit/block.jstabs.php
+++ smarty_bit/block.jstabs.php
@@ -13,12 +13,18 @@
  * Input:		
  * Abstract:	Used to enclose a set of tabs
  */
-function smarty_block_jstabs($params, $content, &$gBitSmarty) {
+function smarty_block_jstabs( $params, $content, &$gBitSmarty ) {
 	global $gBitSystem;
-	$ret  = '<div class="tabpane">'.$content.'</div>';
-	if( $gBitSystem->mPrefs['disable_jstabs'] != 'y' ) {
-		$ret .= "<script type=\"text/javascript\">//<![CDATA[\nsetupAllTabs();\n//]]></script>";
+	extract( $params );
+
+	if( $gBitSystem->mPrefs['disable_jstabs'] == 'y' ) {
+		$ret .= '<div class="tabpane" id="tabby">'.$content.'</div>';
+	} else {
+		$js_before = "<script type=\"text/javascript\">//<![CDATA[\ntabPane = new WebFXTabPane( document.getElementById( 'tabby' ), true );\n//]]></script>";
+		$js_after .= "<script type=\"text/javascript\">//<![CDATA[\nsetupAllTabs();\nvar tabPane;".( !empty( $tab ) ? "\ntabPane.setSelectedIndex( $tab );" : '' )."\n//]]></script>";
+		$ret .= '<div class="tabpane" id="tabby">'.$js_before.$content.$js_after.'</div>';
 	}
+
 	return $ret;
 }
 ?>

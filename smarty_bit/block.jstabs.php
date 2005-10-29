abb5d2e52e5859bffbb58543b1675a547626b381
Max Kremmel <xing@synapse.plus.com>
2005-10-29 15:12:55 +0000

    only add tab id when we want to select a specific tab to reduce number of cookies needed

--- smarty_bit/block.jstabs.php
+++ smarty_bit/block.jstabs.php
@@ -20,12 +20,20 @@ function smarty_block_jstabs( $params, $content, &$gBitSmarty ) {
 	if( $gBitSystem->mPrefs['disable_jstabs'] == 'y' ) {
 		$ret .= '<div class="tabpane">'.$content.'</div>';
 	} else {
-		$id = 1000000 * microtime();
-		$ret .= '<div class="tabpane" id="id_'.$id.'">';
-		$ret .= "<script type=\"text/javascript\">//<![CDATA[\ntabPane = new WebFXTabPane( document.getElementById( 'id_".$id."' ), true );\n//]]></script>";
-		$ret .= $content;
-		$ret .= "<script type=\"text/javascript\">//<![CDATA[\nsetupAllTabs();\nvar tabPane;".( !empty( $tab ) ? "\ntabPane.setSelectedIndex( $tab );" : '' )."\n//]]></script>";
-		$ret .= '</div>';
+		if( !empty( $tab ) ) {
+			$id = 1000000 * microtime();
+			$ret .= '<div class="tabpane" id="id_'.$id.'">';
+			$ret .= "<script type=\"text/javascript\">//<![CDATA[\ntabPane = new WebFXTabPane( document.getElementById( 'id_".$id."' ), true );\n//]]></script>";
+			$ret .= $content;
+			$ret .= "<script type=\"text/javascript\">//<![CDATA[\nsetupAllTabs();var tabPane;".( !empty( $tab ) ? "tabPane.setSelectedIndex( $tab );" : '' )."\n//]]></script>";
+			$ret .= '</div>';
+		} else {
+			$ret .= '<div class="tabpane">';
+			$ret .= $content;
+			$ret .= "<script type=\"text/javascript\">//<![CDATA[\nsetupAllTabs();var tabPane;\n//]]></script>";
+			$ret .= '</div>';
+		}
+
 	}
 
 	return $ret;

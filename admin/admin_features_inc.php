5e32058dd3c491673cd4d3873db2ed42e0ed578a
Michael Jennings <mej@users.sourceforge.net>
2005-12-21 17:58:08 +0000

    Wed Dec 21 12:58:05 2005                        Michael Jennings (mej)
    Fix page link for feature_pretty_urls_extended.
    ----------------------------------------------------------------------

--- admin/admin_features_inc.php
+++ admin/admin_features_inc.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_kernel/admin/admin_features_inc.php,v 1.1.1.1.2.8 2005/12/02 11:31:22 squareing Exp $
+// $Header: /cvsroot/bitweaver/_bit_kernel/admin/admin_features_inc.php,v 1.1.1.1.2.9 2005/12/21 17:58:08 mej Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -19,7 +19,7 @@ $formFeaturesBit = array(
 	'feature_pretty_urls_extended' => array(
 		'label' => 'Use Extended Pretty URLs',
 		'note' => 'In addition to making the URL easier to read and remember it enhances search engine results. Using this feature requires Apache <a href="http://httpd.apache.org/docs-2.0/mod/mod_rewrite.html">mod_rewrite</a> support in the web server (usually installed by default), This extended version adds a /view tag to the URLs to make them unambigious for rewrites.  It will be necessary to modify the default .htaccess files when using this feature.',
-		'page' => 'PrettyUrlsExtended',
+		'page' => 'FeaturePrettyUrlsExtended',
 	),
 	'feature_autolinks' => array(
 		'label' => 'AutoLinks',

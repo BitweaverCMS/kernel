88929d75c6da9d212d97ed75bc911a17ddf0c87f
Max Kremmel <xing@synapse.plus.com>
2006-02-21 12:35:05 +0000

    revive js calendar function

--- smarty_bit/function.jscalendar.php
+++ smarty_bit/function.jscalendar.php
@@ -19,54 +19,49 @@
  * @param array
  * @param Smarty
  * @return string
- */
-function smarty_function_jscalendar($params, &$gBitSmarty)
-{
-    require_once $gBitSmarty->_get_plugin_filepath('shared','make_timestamp');
-    require_once $gBitSmarty->_get_plugin_filepath('function','html_options');
-    /* Default values. */
-    $display_days    = true;
-    $display_months  = true;
-    $display_years   = true;
-    $year_as_text    = false;
-    /* Display years in reverse order? Ie. 2000,1999,.... */
-    $reverse_years   = false;
-    /* Should the select boxes be part of an array when returned from PHP?
-       e.g. setting it to "birthday", would create "birthday[Day]",
-       "birthday[Month]" & "birthday[Year]". Can be combined with prefix */
-    $field_array     = null;
-    /* <select size>'s of the different <select> tags.
-       If not set, uses default dropdown. */
-    /* Unparsed attributes common to *ALL* the <select>/<input> tags.
-       An example might be in the template: all_extra ='class ="foo"'. */
-    $all_extra       = null;
+*/
+function smarty_function_jscalendar($params, &$gBitSmarty) {
+	global $gBitSystem;
+	if( $gBitSystem->isFeatureActive( 'feature_jscalendar' ) ) {
+		//require_once $gBitSmarty->_get_plugin_filepath('shared', 'make_timestamp');
+		//require_once $gBitSmarty->_get_plugin_filepath('function', 'html_options');
 
-    extract($params);
+		// Default values
+		$inputField   = '';      // ID of the input field
+		$fieldFormat  = '%s';    // format of the input field
+		$electric     = 'false'; // ID of the span where the date is to be shown
+		$time         = time();  // override the currently set date
+		$onUpdate     = '';      // execute the following javascript function when a link is pressed
+		$daFormat     = $gBitSystem->getPreference( 'short_date_format' ).' '.$gBitSystem->getPreference( 'short_time_format' ); // format of output date
+		$displayArea  = '';
 
-    global $gBitSystem;
-    $field_format = $gBitSystem->getPreference( "short_date_format" );
-    if ($time != null)
-        $time = $gBitSystem->mServerTimestamp->strftime($field_format, $time);
+		// override default values
+		extract( $params );
 
-    if ($readonly) {
-        $html_result = $time;
-    } else { 
-        $html_result = "<input type=\"text\" name=\"".$field_array."\" id=\"".$field_array."\" value=\"".$time."\" />
-                <img src=\"".JSCALENDAR_PKG_URL."img.gif\" id=\"f_trigger_".$field_array."\" style=\"cursor: pointer;\" title=\"Date selector\" />
-                                <script type=\"text/javascript\">//<![CDATA[
-    Calendar.setup({
-        inputField     :    \"".$field_array."\",     // id of the input field
-        ifFormat       :    \"".$field_format."\",      // format of the input field
-        button         :    \"f_trigger_".$field_array."\",  // trigger for the calendar (button ID)
-        align          :    \"Tl\",           // alignment (defaults to \"Bl\")
-        singleClick    :    true
-    });
-//]]></script>";
-    }
+		$time = $gBitSystem->mServerTimestamp->getDisplayDateFromUTC( $time );
+		$time = strftime( "%m/%d/%Y %H:%M", $time );
 
-    return $html_result;
+		if( $readonly ) {
+			$html_result = $time;
+		} else {
+			$html_result =
+				"<script type=\"text/javascript\">//<![CDATA[
+					Calendar.setup({
+						date        : \"$time\",
+						inputField  : \"$inputField\",
+						ifFormat    : \"$fieldFormat\",
+						daFormat    : \"$daFormat\",
+						displayArea : \"$displayArea\",
+						electric    : $electric,
+						onUpdate    : $onUpdate
+					});
+				//]]></script>"
+			;
+		}
+
+		return $html_result;
+	} else {
+		return '';
+	}
 }
-
-/* vim: set expandtab: */
-
 ?>

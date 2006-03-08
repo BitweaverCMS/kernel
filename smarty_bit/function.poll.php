56164a54898ab1170184b40277efa43fb59fd1d3
Stephan Borg <wolff_borg@users.sourceforge.net>
2006-03-08 11:55:20 +0000

    Fixed Poll smarty plugin and created liberty plugin

--- smarty_bit/function.poll.php
+++ smarty_bit/function.poll.php
@@ -14,27 +14,29 @@ require_once( KERNEL_PKG_PATH.'BitBase.php' );
  * smarty_function_poll
  */
 function smarty_function_poll($params, &$gBitSmarty) {
-    global $polllib, $gBitSystem;
+    global $gBitSystem;
+    require_once( POLLS_PKG_PATH.'poll_lib.php' );
+    if(!isset($polllib)) {
+      $polllib = new PollLib();
+    }
 
     extract($params);
     // Param = zone
-	include_once( POLLS_PKG_PATH.'poll_lib.php' );
-	include_once( LIBERTY_PKG_PATH.'LibertyComment.php' );
 
     if (empty($id)) {
       $id = $polllib->get_random_active_poll();
     }
     if($id) {
-      $menu_info = $polllib->get_poll($id);
-      $channels = $polllib->list_poll_options($id,0,-1,'option_id_asc','');
+      $poll_info = $polllib->get_poll($id);
+      $polls = $polllib->list_poll_options($id,0,-1,'option_id_asc','');
 			if ($gBitSystem->getPreference('feature_poll_comments') == 'y') {
+                                include_once( LIBERTY_PKG_PATH.'LibertyComment.php' );
 				$comments = new LibertyComment();
-				$comments_count = $comments->count_comments("poll:".$menu_info["poll_id"]);
+				$comments_count = $comments->count_comments("poll:".$poll_info["poll_id"]);
 			}
 			$gBitSmarty->assign('comments', $comments_count);
-      $gBitSmarty->assign('ownurl',POLLS_PKG_URL.'results.php?poll_id='.$id);
-      $gBitSmarty->assign('menu_info',$menu_info);
-      $gBitSmarty->assign('channels',$channels["data"]);
+      $gBitSmarty->assign('poll_info',$poll_info);
+      $gBitSmarty->assign('polls',$polls["data"]);
       $gBitSmarty->display('bitpackage:polls/poll.tpl');
     }
 }

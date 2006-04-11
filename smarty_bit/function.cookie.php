347e72d017d02851280f130dc5d8b53223360ec7
Max Kremmel <xing@synapse.plus.com>
2006-04-11 12:07:54 +0000

    fix some of the fortune cookie code and move out of the tikiwiki parser to a seperate data plugin

--- smarty_bit/function.cookie.php
+++ smarty_bit/function.cookie.php
@@ -8,19 +8,9 @@
 /**
  * smarty_function_cookie
  */
-function smarty_function_cookie($params, &$gBitSmarty)
-{
+function smarty_function_cookie($params, &$gBitSmarty) {
     global $taglinelib;
     include_once( TIDBITS_PKG_PATH.'BitFortuneCookies.php' );
-    extract($params);
-    // Param = zone
-  
-  	if( $taglinelib ) {
-	    $data = $taglinelib->pick_cookie();
-   		print($data);
-	}
+	print( $taglinelib->pick_cookie() );
 }
-
-/* vim: set expandtab: */
-
 ?>

92d1a8e9b25fa77aa2922a5d5617e59ee3e4f234
Max Kremmel <xing@synapse.plus.com>
2006-04-11 09:43:25 +0000

    move banning permission to tidbits

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -118,7 +118,6 @@ $gBitInstaller->registerModules( $moduleHash );
 // ### Default UserPermissions
 $gBitInstaller->registerUserPermissions( KERNEL_PKG_NAME, array(
 	array('bit_p_admin', 'Can manage users groups and permissions and all aspects of site management', 'admin', KERNEL_PKG_NAME ),
-	array('bit_p_admin_banning', 'Can ban users or IPs', 'admin', KERNEL_PKG_NAME),
 	array('bit_p_access_closed_site', 'Can access site when closed', 'admin', KERNEL_PKG_NAME)
 ) );
 

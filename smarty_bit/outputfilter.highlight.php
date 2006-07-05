f881a8d5fb873ed36237a1ccebc7778cade83303
Max Kremmel <xing@synapse.plus.com>
2006-07-05 17:57:32 +0000

    positively select text that needs highlighting and replace repetitive code with loops. this approach should also reduce load somewhat. perhaps we need to fiddle with the regex to allow for missing <!-- end .body --> html comments

--- smarty_bit/outputfilter.highlight.php
+++ smarty_bit/outputfilter.highlight.php
@@ -26,102 +26,65 @@
  *           patched by mose <mose@feu.org>
  * -------------------------------------------------------------
  */
- function smarty_outputfilter_highlight($source, &$gBitSmarty) {
- 	
-    $highlight = $_REQUEST['highlight']; 
-    $words = $highlight;
-    if (!isset($highlight) || empty($highlight)) {
-			return $source;
-    }
+function smarty_outputfilter_highlight( $source, &$gBitSmarty ) {
+	// This array is used to choose colours for supplied highlight terms
+	$colorArr = array( '#ffff66', '#ff9999', '#a0ffff', '#ff66ff', '#99ff99' );
 
-   	//  Pull everything up to bitweaver's <div class="body" tag
-	preg_match_all("!.*?.\<div class=.?body.?!si", $source, $match);
-	$_bwbody_blocks = $match[0];
-    $source = preg_replace("!.*?.\<div class=.?body.?!si",
-    '@@@SMARTY:TRIM:BWBODY@@@', $source);
-	
-	//  Pull everything past <!-- end .body -->
-	preg_match_all("!<.?.?.? end .body .?.?>.*!si", $source, $match);
-	$_bwendbody_blocks = $match[0];
-    $source = preg_replace("!<.?.?.? end .body .?.?>.*!si",
-    '@@@SMARTY:TRIM:BWENDBODY@@@', $source);
-	
-	// let's try killing everything in module blocks
-    preg_match_all("!<div class=?.boxtitle.?[^>]+>.*?</div>!si", $source, $match);
-    $_boxtitle_blocks = $match[0];
-    $source = preg_replace("!<div class=?.boxtitle.?[^>]+>.*?</div>!si",
-    '@@@SMARTY:TRIM:BOXTITLE@@@', $source);
-	
-	// let's try killing everything in module blocks
-    preg_match_all("!<div class=?.boxcontent.?[^>]+>.*?</div>!si", $source, $match);
-    $_boxcontent_blocks = $match[0];
-    $source = preg_replace("!<div class=?.boxcontent.?[^>]+>.*?</div>!si",
-    '@@@SMARTY:TRIM:BOXCONTENT@@@', $source);
-	
-	// remove everything in the TOC
-    preg_match_all("!<div class=?.maketoc.?[^>]+>.*?</div>!si", $source, $match);
-    $_maketoc_blocks = $match[0];
-    $source = preg_replace("!<div class=?.maketoc.?[^>]+>.*?</div>!si",
-    '@@@SMARTY:TRIM:MAKETOC@@@', $source);
-	
-	// Pull out the script blocks
-    preg_match_all("!<script[^>]+>.*?</script>!is", $source, $match);
-    $_script_blocks = $match[0];
-    $source = preg_replace("!<script[^>]+>.*?</script>!is",
-    '@@@SMARTY:TRIM:SCRIPT@@@', $source);
+	$words = $_REQUEST['highlight'];
+	if( empty( $words ) ) {
+		return $source;
+	}
 
-    // pull out all html tags
-    preg_match_all("'<[\/\!]*?[^<>]*?>'si", $source, $match);
-    $_tag_blocks = $match[0];
-    $source = preg_replace("'<[\/\!]*?[^<>]*?>'si", '@@@SMARTY:TRIM:TAG@@@', $source);
+	// get all text that needs to be highlighted
+	$extractor = "#<div\s+class=.?body[^>]*>.*?<!--\s*end\s*\.?body\s*-->#is";
+	preg_match_all( $extractor, $source, $match );
+	$highlights = $match[0];
 
-    // This array is used to choose colors for supplied highlight terms
-    $colorArr = array('#ffff66','#ff9999','#A0FFFF','#ff66ff','#99ff99');
+	$ret = array();
+	foreach( $highlights as $key => $highlight ) {
+		// if we picked something up, we highlight the contents
+		if( !empty( $highlight ) ) {
+			$source = preg_replace( $extractor, "@@@SMARTY:TRIM:HIGHLIGHT@@@", $source );
 
-    // Wrap all the highlight words with tags bolding them and changing
-    // their background colors
-    $wordArr = split(" ",$words);
-    $i = 0;
-    foreach($wordArr as $word) {
-			$source = preg_replace("'($word)'si", '<span style="color:black;background-color:'.$colorArr[$i].';">$1</span>', $source); 
-			$i++;
-    }
+			// extraction patterns and their replacements
+			$patterns = array(
+				"!<script[^>]+>.*?</script>!is"           => "@@@SMARTY:TRIM:SCRIPT@@@",
+				"!<div class=.?maketoc[^>]*>.*?</div>!si" => "@@@SMARTY:TRIM:MAKETOC@@@",
+				"'<[\/\!]*?[^<>]*?>'si"                   => "@@@SMARTY:TRIM:TAG@@@",
+			);
 
-	
-	// replace the document all the way up to the <div class="body"
-    foreach($_bwbody_blocks as $curr_block) {
-            $source = preg_replace("!@@@SMARTY:TRIM:BWBODY@@@!",$curr_block,$source,1);
-    }
-	
-	// replace the document from <!-- end .body -->
-    foreach($_bwendbody_blocks as $curr_block) {
-            $source = preg_replace("!@@@SMARTY:TRIM:BWENDBODY@@@!",$curr_block,$source,1);
-    }
-	
-	// replace the boxtitle
-    foreach($_boxtitle_blocks as $curr_block) {
-            $source = preg_replace("!@@@SMARTY:TRIM:BOXTITLE@@@!",$curr_block,$source,1);
-    }
-	
-	// replace the boxtitle
-    foreach($_boxcontent_blocks as $curr_block) {
-            $source = preg_replace("!@@@SMARTY:TRIM:BOXCONTENT@@@!",$curr_block,$source,1);
-    }
+			ksort( $patterns );
 
-	// replace the TOC
-    foreach($_maketoc_blocks as $curr_block) {
-            $source = preg_replace("!@@@SMARTY:TRIM:MAKETOC@@@!",$curr_block,$source,1);
-    }
-	
-	// replace script blocks
-    foreach($_script_blocks as $curr_block) {
-			$source = preg_replace("!@@@SMARTY:TRIM:SCRIPT@@@!",$curr_block,$source,1);
-    }
-	
-	foreach($_tag_blocks as $curr_block) {
-			$source = preg_replace("!@@@SMARTY:TRIM:TAG@@@!",$curr_block,$source,1);
-    }
+			foreach( $patterns as $pattern => $replace ) {
+				preg_match_all( $pattern, $highlight, $match );
+				$matches[$replace] = $match[0];
+				$highlight = preg_replace( $pattern, $replace, $highlight );
+			}
 
-    return $source; 
- }
+			$i = 0;
+			// Wrap all the highlight words with a colourful span
+			$wordArr = split( " ", urldecode( $words ) );
+			foreach( $wordArr as $word ) {
+				$highlight = preg_replace( "'($word)'si", '<span style="font-weight:bold;padding:0 0.3em;color:black;background-color:'.$colorArr[$i++].';">$1</span>', $highlight ); 
+			}
+
+			krsort( $patterns );
+
+			foreach( $patterns as $pattern ) {
+				foreach( $matches[$pattern] as $insert ) {
+					$highlight = preg_replace( "!{$pattern}!", $insert, $highlight, 1 );
+				}
+			}
+
+			$ret[] = $highlight;
+		}
+	}
+
+	// insert the highlighted code back into the source
+	foreach( $ret as $highlight ) {
+		$source = preg_replace( "!@@@SMARTY:TRIM:HIGHLIGHT@@@!", $highlight, $source, 1 );
+	}
+
+	return $source; 
+}
 ?>

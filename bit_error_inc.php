aa81e6f418daad9cecaedd029a96d5743ebd7464
Wakeworks <wakeworks@users.sourceforge.net>
2006-07-14 19:20:18 +0000

    fix ERROR_EMAIL parens

--- bit_error_inc.php
+++ bit_error_inc.php
@@ -24,7 +24,7 @@ if( !defined( 'BIT_INSTALL' ) &&  !defined( 'ADODB_ERROR_HANDLER' )  ) {
 
 function bit_log_error( $pLogMessage, $pSubject, $pFatal = TRUE ) {
 	// You can prevent sending of error emails by adding define('ERROR_EMAIL', ''); in your config_inc.php
-	$errorEmail = defined( 'ERROR_EMAIL' ) ? ERROR_EMAIL : !empty( $_SERVER['SERVER_ADMIN'] ) ? $_SERVER['SERVER_ADMIN'] : NULL;
+	$errorEmail = defined( 'ERROR_EMAIL' ) ? ERROR_EMAIL : (!empty( $_SERVER['SERVER_ADMIN'] ) ? $_SERVER['SERVER_ADMIN'] : NULL);
 
 	error_log( $pLogMessage );
 

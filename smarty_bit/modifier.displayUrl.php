8cfb7aa3f560ad52dff821becffe1c57c44a12b9
Sylvie Greverend <sylvieg@users.sourceforge.net>
2006-07-31 18:42:11 +0000

    some fixes around displayUrl

--- smarty_bit/modifier.displayUrl.php
+++ smarty_bit/modifier.displayUrl.php
@@ -10,12 +10,27 @@
  * -------------------------------------------------------------
  * Type:     modifier
  * Name:     displayUrl
- * Purpose:  give bqck the displal Url
+ * Purpose:  give back the display Url of an obect
+ * Example: {'My Page'|displayUrl}, {'admin'|displyUrl:BitUser}, {$gContent|displayUrl:MyObject}
  * -------------------------------------------------------------
  */
 function smarty_modifier_displayUrl($string, $lib='BitPage') {
-	return BitPage::getDisplayUrl($string);  
-
+	switch ($lib) {
+	case 'BitPage':
+		require_once(WIKI_PKG_PATH.'BitPage.php'); 
+		return BitPage::getDisplayUrl($string);
+	case 'BitUser':
+		require_once(USERS_PKG_PATH.'BitUser.php'); 
+		return BitUser::getDisplayUrl($string);
+	default:
+		if (is_array($string) && !empty($string['display_url'])) {
+			return $string['display_url'];
+		} else if (is_object($string) && method_exists($string, 'getDisplayUrl')) {
+			return $string->getDisplayUrl();
+		} else {
+			return LibertyContent::getDisplayUrl(null, $string);
+		}
+	}
 }
 
 ?>

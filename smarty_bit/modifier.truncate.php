5112d997b6ccbe56c530db66c4e9ec900b08350d
Sylvie Greverend <sylvieg@users.sourceforge.net>
2006-08-17 12:21:37 +0000

    additional parameter to smarty truncate to be able to expand the ... by clicking on it with the rest of the text

--- smarty_bit/modifier.truncate.php
+++ smarty_bit/modifier.truncate.php
@@ -20,10 +20,11 @@
  * @param integer
  * @param string
  * @param boolean
+ * @param string
  * @return string
  */
 function smarty_modifier_truncate($string, $length = 80, $etc = '...',
-                                  $break_words = false)
+                                  $break_words = false, $divId='')
 {
     if ($length == 0)
         return '';
@@ -36,6 +37,10 @@ function smarty_modifier_truncate($string, $length = 80, $etc = '...',
             $fragment = mb_substr($string, 0, $length + 1);
             $fragment = preg_replace('/\s+(\S+)?$/', '', $fragment);
         }
+	if (!empty($divId)) {
+		$etc = "<span  style='display:inline;' id='dyn_".$divId."_display'><a class='truncate' onclick='javascript:toggle_dynamic_var(\"$divId\");' title='".tra('Click to see the full text')."'>$etc</a></span>";
+		$etc .= "<span style='display:none;' id='dyn_".$divId."_edit'>".substr($string, strlen($fragment))."</span>";
+	}
 	return $fragment.$etc;
     } else
         return $string;

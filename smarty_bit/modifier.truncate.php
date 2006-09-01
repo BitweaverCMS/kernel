3161901c1691a7a9242a11e507e0ab84aa2e08c3
Max Kremmel <xing@synapse.plus.com>
2006-09-01 10:16:20 +0000

    cleanup

--- smarty_bit/modifier.truncate.php
+++ smarty_bit/modifier.truncate.php
@@ -23,27 +23,25 @@
  * @param string
  * @return string
  */
-function smarty_modifier_truncate($string, $length = 80, $etc = '...',
-                                  $break_words = false, $divId='')
-{
-    if ($length == 0)
-        return '';
+function smarty_modifier_truncate($string, $length = 80, $etc = '...', $break_words = false, $divId='') {
+	if ($length == 0)
+		return '';
 
-    if (strlen($string) > $length) {
-        $length -= strlen($etc);	
-       if ($break_words) // break the word
-        	$fragment = mb_substr($string, 0, $length);
-        else {
-            $fragment = mb_substr($string, 0, $length + 1);
-            $fragment = preg_replace('/\s+(\S+)?$/', '', $fragment);
-        }
-	if (!empty($divId)) {
-		$etc = "<span  style='display:inline;' id='dyn_".$divId."_display'><a class='truncate' onclick='javascript:toggle_dynamic_var(\"$divId\");' title='".tra('Click to see the full text')."'>$etc</a></span>";
-		$etc .= "<span style='display:none;' id='dyn_".$divId."_edit'>".substr($string, strlen($fragment))."</span>";
-	}
-	return $fragment.$etc;
-    } else
-        return $string;
+	if (strlen($string) > $length) {
+		$length -= strlen($etc);
+		if ($break_words) // break the word
+			$fragment = mb_substr($string, 0, $length);
+		else {
+			$fragment = mb_substr($string, 0, $length + 1);
+			$fragment = preg_replace('/\s+(\S+)?$/', '', $fragment);
+		}
+		if (!empty($divId)) {
+			$etc = "<span  style='display:inline;' id='dyn_".$divId."_display'><a class='truncate' onclick='javascript:toggle_dynamic_var(\"$divId\");' title='".tra('Click to see the full text')."'>$etc</a></span>";
+			$etc .= "<span style='display:none;' id='dyn_".$divId."_edit'>".substr($string, strlen($fragment))."</span>";
+		}
+		return $fragment.$etc;
+	} else
+		return $string;
 }
 
 /* vim: set expandtab: */

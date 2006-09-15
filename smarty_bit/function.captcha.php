e6473b4d9ac63afd6b4f9a926ad9beb467ace41d
Christian Fowler <spider@viovio.com>
2006-09-15 21:27:35 +0000

    add permission checking to only display captcha for unperm'ed people

--- smarty_bit/function.captcha.php
+++ smarty_bit/function.captcha.php
@@ -9,20 +9,23 @@
  * smarty_function_rss
  */
 function smarty_function_captcha($params, &$gBitSmarty) {
-	global $gLibertySystem;
-	$size = !empty( $params['size'] ) ? $params['size'] : '5';
-	$getString = 'size='.$size;
-	if( @BitBase::verifyId( $params['width'] ) ) {
-		$getString .= '&width='.$params['width'];
+	global $gLibertySystem, $gBitUser;
+	if( !$gBitUser->hasPermission( 'p_users_bypass_captcha' ) ) {
+		$size = !empty( $params['size'] ) ? $params['size'] : '5';
+		$getString = 'size='.$size;
+		if( @BitBase::verifyId( $params['width'] ) ) {
+			$getString .= '&width='.$params['width'];
+		}
+		if( @BitBase::verifyId( $params['height'] ) ) {
+			$getString .= '&height='.$params['height'];
+		}
+		$ret = '
+			<div class="captcha">
+				<img src="'.USERS_PKG_URL.'captcha_image.php?'.$getString.'" alt="'.tra('Random Image').'" align="top"/><input type="text" name="captcha" size="'.$size.'"/>
+				<br/><em class="small">'.tra( 'Spam Protection: Please copy the code into the box.' ).'</em>
+			</div>
+		'; 
+		print $ret;
 	}
-	if( @BitBase::verifyId( $params['height'] ) ) {
-		$getString .= '&height='.$params['height'];
-	}
-	$ret = '
-		<div class="captcha">
-			<img src="'.USERS_PKG_URL.'captcha_image.php?'.$getString.'" alt="'.tra('Random Image').'"/><input type="text" name="captcha" size="'.$size.'"/>
-		</div>
-	'; 
-	print $ret;
 }
 ?>

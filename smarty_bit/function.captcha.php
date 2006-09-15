fbf6701157f7de63d044be1b79974c7596278f58
Christian Fowler <spider@viovio.com>
2006-09-15 21:07:42 +0000

    simple function captcha now working for shoutbox

--- smarty_bit/function.captcha.php
+++ smarty_bit/function.captcha.php
@@ -10,7 +10,12 @@
  */
 function smarty_function_captcha($params, &$gBitSmarty) {
 	global $gLibertySystem;
-	$ret = '<img src="'.USERS_PKG_URL.'captcha_image.php" alt="'.tra('Random Image').'"/>...'; 
+	$size = !empty( $params['size'] ) ? $params['size'] : '5';
+	$ret = '
+		<div class="captcha">
+			<img src="'.USERS_PKG_URL.'captcha_image.php" alt="'.tra('Random Image').'"/><input type="text" name="captcha" size="'.$size.'"/>
+		</div>
+	'; 
 	print $ret;
 }
 ?>

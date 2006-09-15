45cd28a6bc6118ecb129e16e39137ae75dc7413b
Christian Fowler <spider@viovio.com>
2006-09-15 22:10:39 +0000

    add captcha to liberty comments

--- smarty_bit/function.captcha.php
+++ smarty_bit/function.captcha.php
@@ -19,12 +19,33 @@ function smarty_function_captcha($params, &$gBitSmarty) {
 		if( @BitBase::verifyId( $params['height'] ) ) {
 			$getString .= '&height='.$params['height'];
 		}
-		$ret = '
-			<div class="captcha">
-				<img src="'.USERS_PKG_URL.'captcha_image.php?'.$getString.'" alt="'.tra('Random Image').'" align="top"/><input type="text" name="captcha" size="'.$size.'"/>
-				<br/><em class="small">'.tra( 'Spam Protection: Please copy the code into the box.' ).'</em>
-			</div>
-		'; 
+		$style = !empty( $params['style'] ) ? $params['style'] : 'condensed';
+		switch( $style ) {
+			case 'row':
+				$ret = '
+<div class="row">
+	<div class="formlabel">
+		'.tra( 'Verification code' ).'
+		<img src="'.USERS_PKG_URL.'captcha_image.php?'.$getString.'" alt="'.tra('Random Image').'" align="top"/><br/>
+	</div>
+	<div class="forminput">
+		<br/>
+		<input type="text" name="captcha" id="captcha" size="'.$size.'"/>
+		<div class="formhelp">'.tra( 'Spam Protection: Please copy the code into the box.' ).'</div>
+	</div>
+</div>
+				';
+				break;
+			default:
+				// condensed
+				$ret = '
+					<div class="captcha">
+						<img src="'.USERS_PKG_URL.'captcha_image.php?'.$getString.'" alt="'.tra('Random Image').'" align="top"/><input type="text" name="captcha" size="'.$size.'"/>
+						<br/><em class="small">'.tra( 'Spam Protection: Please copy the code into the box.' ).'</em>
+					</div>
+				'; 
+				break;
+			}
 		print $ret;
 	}
 }

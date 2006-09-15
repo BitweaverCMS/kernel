9e69e106eb69e1a4840fae9f2580f2b95f542e1d
Christian Fowler <spider@viovio.com>
2006-09-15 21:16:56 +0000

    smarty captcha plugin is now configurable with height, width, and size params

--- smarty_bit/function.captcha.php
+++ smarty_bit/function.captcha.php
@@ -11,9 +11,16 @@
 function smarty_function_captcha($params, &$gBitSmarty) {
 	global $gLibertySystem;
 	$size = !empty( $params['size'] ) ? $params['size'] : '5';
+	$getString = 'size='.$size;
+	if( @BitBase::verifyId( $params['width'] ) ) {
+		$getString .= '&width='.$params['width'];
+	}
+	if( @BitBase::verifyId( $params['height'] ) ) {
+		$getString .= '&height='.$params['height'];
+	}
 	$ret = '
 		<div class="captcha">
-			<img src="'.USERS_PKG_URL.'captcha_image.php" alt="'.tra('Random Image').'"/><input type="text" name="captcha" size="'.$size.'"/>
+			<img src="'.USERS_PKG_URL.'captcha_image.php?'.$getString.'" alt="'.tra('Random Image').'"/><input type="text" name="captcha" size="'.$size.'"/>
 		</div>
 	'; 
 	print $ret;

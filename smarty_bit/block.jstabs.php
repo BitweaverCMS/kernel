8b6d279611a062c6ed09336318f733d46eaf9767
Max Kremmel <xing@synapse.plus.com>
2006-12-27 07:43:54 +0000

    add option to directly spcify the jstab in the url using the numeric index of the tab beginning with 0. usage: my.domain.com/path/to/page.php?jstab=2

--- smarty_bit/block.jstabs.php
+++ smarty_bit/block.jstabs.php
@@ -20,12 +20,16 @@ function smarty_block_jstabs( $params, $content, &$gBitSmarty ) {
 	if( $gBitSystem->isFeatureActive( 'site_disable_jstabs' ) ) {
 		$ret .= '<div class="tabpane">'.$content.'</div>';
 	} else {
-		if( !empty( $tab ) ) {
+		if( !empty( $tab ) || !empty( $_REQUEST['jstab'] ) ) {
+			// make sure we aren't passed any evil shit
+			if( empty( $tab ) && !empty( $_REQUEST['jstab'] ) && preg_match( "!^\d+$!", $_REQUEST['jstab'] ) ) {
+				$tab = $_REQUEST['jstab'];
+			}
 			$id = 1000000 * microtime();
 			$ret .= '<div class="tabpane" id="id_'.$id.'">';
 			$ret .= "<script type=\"text/javascript\">/*<![CDATA[*/ tabPane = new WebFXTabPane( $( 'id_".$id."' ), true ); /*]]>*/</script>";
 			$ret .= $content;
-			$ret .= "<script type=\"text/javascript\">/*<![CDATA[*/ setupAllTabs();var tabPane;".( !empty( $tab ) ? "tabPane.setSelectedIndex( $tab );" : '' )." /*]]>*/</script>";
+			$ret .= "<script type=\"text/javascript\">/*<![CDATA[*/ setupAllTabs();var tabPane;tabPane.setSelectedIndex( $tab );/*]]>*/</script>";
 			$ret .= '</div>';
 		} else {
 			$ret .= '<div class="tabpane">';

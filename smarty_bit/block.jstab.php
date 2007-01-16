e4037ac057ba6e71f5df84e30358df90cd4d9090
Max Kremmel <xing@synapse.plus.com>
2007-01-16 11:35:04 +0000

    only display tab stuff when we have more than one tab to display - useful for dynamically inserted tabs by services

--- smarty_bit/block.jstab.php
+++ smarty_bit/block.jstab.php
@@ -13,10 +13,11 @@
  * Input:
  * Abstract:	Used to enclose a set of tabs
  */
-function smarty_block_jstab($params, $content, &$gBitSmarty) {
+function smarty_block_jstab( $pParams, $pContent, &$gBitSmarty ) {
+	// if this is modified, please adjust the preg_match_all() pattern in block.jstabs.php
 	$ret  = '<div class="tabpage">';
-	$ret .= '<h4 class="tab">'.tra( isset( $params['title'] ) ? $params['title'] : 'No Title' ).'</h4>';
-	$ret .= $content;
+	$ret .= '<h4 class="tab">'.tra( isset( $pParams['title'] ) ? $pParams['title'] : 'No Title' ).'</h4>';
+	$ret .= $pContent;
 	$ret .= '</div>';
 	return $ret;
 }
--- smarty_bit/block.jstabs.php
+++ smarty_bit/block.jstabs.php
@@ -13,12 +13,21 @@
  * Input:		
  * Abstract:	Used to enclose a set of tabs
  */
-function smarty_block_jstabs( $params, $content, &$gBitSmarty ) {
+function smarty_block_jstabs( $pParams, $pContent, &$gBitSmarty ) {
 	global $gBitSystem;
-	extract( $params );
+	extract( $pParams );
 
+	// Work out if we want to insert tabs at all on this page
+	// This is necessary since we insert tabs dynamically using services
+	preg_match_all( '#<div class="tabpage">#', $pContent, $tabs );
+	if( !empty( $tabs[0] ) && count( $tabs[0] ) <= 1 ) {
+		$pContent = preg_replace( "#<h4[^>]*tab.*?</h4>#", '', $pContent );
+		return $pContent;
+	}
+
+	// When tabs are disabled, we simply wrap the tabs with the appropriate div for styling
 	if( $gBitSystem->isFeatureActive( 'site_disable_jstabs' ) ) {
-		$ret .= '<div class="tabpane">'.$content.'</div>';
+		$ret .= '<div class="tabpane">'.$pContent.'</div>';
 	} else {
 		if( !empty( $tab ) || !empty( $_REQUEST['jstab'] ) ) {
 			// make sure we aren't passed any evil shit
@@ -28,16 +37,15 @@ function smarty_block_jstabs( $params, $content, &$gBitSmarty ) {
 			$id = 1000000 * microtime();
 			$ret .= '<div class="tabpane" id="id_'.$id.'">';
 			$ret .= "<script type=\"text/javascript\">/*<![CDATA[*/ tabPane = new WebFXTabPane( $( 'id_".$id."' ), true ); /*]]>*/</script>";
-			$ret .= $content;
+			$ret .= $pContent;
 			$ret .= "<script type=\"text/javascript\">/*<![CDATA[*/ setupAllTabs();var tabPane;tabPane.setSelectedIndex( $tab );/*]]>*/</script>";
 			$ret .= '</div>';
 		} else {
 			$ret .= '<div class="tabpane">';
-			$ret .= $content;
+			$ret .= $pContent;
 			$ret .= "<script type=\"text/javascript\">/*<![CDATA[*/ setupAllTabs();var tabPane; /*]]>*/</script>";
 			$ret .= '</div>';
 		}
-
 	}
 
 	return $ret;

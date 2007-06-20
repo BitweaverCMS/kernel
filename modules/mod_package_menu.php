05593530ab73b80d01bc263005756fe8c3d52e78
Max Kremmel <xing@synapse.plus.com>
2007-06-20 11:10:23 +0000

    display admin menu when viewing any /admin/ page

--- modules/mod_package_menu.php
+++ modules/mod_package_menu.php
@@ -5,19 +5,17 @@
  */
 global $gBitSystem;
 
-if( ACTIVE_PACKAGE == 'messages' ) {
-	$active = 'users';
-} else {
-	$active = ACTIVE_PACKAGE;
-}
+// if we're on any page in an admin/ dir, we'll simply set this to kernel
+$admin = preg_match( "!/admin/!", $_SERVER['PHP_SELF'] );
 
-if( !empty( $gBitSystem->mAppMenu[$active]['menu_template'] ) ) {
-	$gBitSmarty->assign( 'packageMenu', $gBitSystem->mAppMenu[$active] );
+if( !empty( $gBitSystem->mAppMenu[ACTIVE_PACKAGE]['menu_template'] ) && !$admin ) {
+	$gBitSmarty->assign( 'packageMenu', $gBitSystem->mAppMenu[ACTIVE_PACKAGE] );
 }
 
 if( empty( $module_title )) {
 	$pkgName = constant( strtoupper( ACTIVE_PACKAGE ).'_PKG_NAME' );
-	if( $pkgName == 'kernel' ) {
+
+	if( $pkgName == 'kernel' || $admin ) {
 		$pkgName = 'Administration';
 	}
 	$title = $gBitSystem->getConfig( $pkgName."_menu_text", ucfirst( $pkgName ));

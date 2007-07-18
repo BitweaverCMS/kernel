abe5a7c4ab398c4616189ba8b494fbff761fb565
Max Kremmel <xing@synapse.plus.com>
2007-07-18 20:38:38 +0000

    clean out some ancient code and (finally) sort packages in admin page

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_kernel/admin/index.php,v 1.16 2007/04/04 18:07:15 squareing Exp $
+// $Header: /cvsroot/bitweaver/_bit_kernel/admin/index.php,v 1.17 2007/07/18 20:38:38 squareing Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -10,43 +10,20 @@
 global $gForceAdodb;
 $gForceAdodb = TRUE;
 require_once( '../../bit_setup_inc.php' );
+require_once( KERNEL_PKG_PATH.'simple_form_functions_lib.php' );
 
-if( isset( $_REQUEST["page"] ) ) {
+$gBitSystem->verifyPermission( 'p_admin' );
+
+if( isset( $_REQUEST["page"] )) {
 	$page = $_REQUEST["page"];
-	$pageName = preg_replace('/_/', ' ', $page);
+	$pageName = preg_replace( '/_/', ' ', $page );
 	$gBitSystem->setBrowserTitle( "$pageName Settings" );
 } else {
 	$gBitSystem->setBrowserTitle( 'Administration' );
 }
 
-$gBitSystem->verifyPermission( 'p_admin' );
-
-require_once( KERNEL_PKG_PATH.'simple_form_functions_lib.php' );
-
-//make an alias in case anyone decides to verifyInstalledPackages
-$gBitInstaller = &$gBitSystem;
-//$gBitSystem->verifyInstalledPackages();
-
-$home_blog = $gBitSystem->getConfig("home_blog", 0);
-$gBitSmarty->assign('home_blog', $home_blog);
-
-$home_forum = $gBitSystem->getConfig("home_forum", 0);
-$gBitSmarty->assign('home_forum', $home_forum);
-
-$home_gallery = $gBitSystem->getConfig("home_gallery", 0);
-$gBitSmarty->assign('home_gallery', $home_gallery);
-if( isset( $page ) ) {
-	$gBitSmarty->assign('page', $page);
-}
-if( isset( $pageName ) ) {
-	$gBitSmarty->assign( 'pageName', $pageName );
-}
-
-$home_file_gallery = $gBitSystem->getConfig("home_file_gallery", 0);
-$gBitSmarty->assign('home_file_gallery', $home_file_gallery);
-
-if (isset($page)) {
-	if( preg_match('/\.php/', $page ) ) {
+if( !empty( $page )) {
+	if( preg_match( '/\.php/', $page )) {
 		$adminPage = $page;
 	} else {
 		$file = $page; // Default file name
@@ -79,28 +56,27 @@ if (isset($page)) {
 
 		$adminPage =  constant( strtoupper( $package ).'_PKG_PATH' ).'/admin/admin_'.$file.'_inc.php';
 	}
-	$gBitSmarty->assign('package', $package );
-	$gBitSmarty->assign('file', $file );
+	$gBitSmarty->assign( 'package', $package );
+	$gBitSmarty->assign( 'file', $file );
 	include_once ( $adminPage );
+
 	// Spiderr - a bit hackish, but need to force preferences refresh
 	$gBitSystem->loadConfig();
 } else {
-//vd( $gBitSystem->mPackages );
 	$adminTemplates = array();
-	foreach( array_keys( $gBitSystem->mPackages ) as $package ) {
+	// deal with package sorting for a unified layout
+	$packages = array_keys( $gBitSystem->mPackages );
+	asort( $packages );
+	$packages = array_unique( array_merge( array( 'kernel', 'liberty', 'users', 'themes' ), $packages ));
+	foreach( $packages as $package ) {
 		$lowerPackage = strtolower( $package );
-		$upperPackage = strtoupper( $package );
-		$tpl = "bitpackage:$lowerPackage/menu_".$lowerPackage."_admin.tpl";
-		if( ($gBitSystem->isPackageActive( $package ) || $lowerPackage == 'kernel') && @$gBitSmarty->template_exists( $tpl ) ) {
+		$tpl = "bitpackage:$lowerPackage/menu_{$lowerPackage}_admin.tpl";
+		if(( $gBitSystem->isPackageActive( $package ) || $lowerPackage == 'kernel' ) && @$gBitSmarty->template_exists( $tpl )) {
 			$adminTemplates[$package] = $tpl;
 		}
 	}
-	$gBitSmarty->assign_by_ref( 'kernelTemplate', $adminTemplates["kernel"] );
 	$gBitSmarty->assign_by_ref( 'adminTemplates', $adminTemplates );
 }
-
-$admin_panels = $gBitSystem->mAppMenu;
-$gBitSmarty->assign('admin_panels', $admin_panels);
 
 if( !empty( $_REQUEST['version_check'] ) ) {
 	$gBitSmarty->assign( 'version_info', $gBitSystem->checkBitVersion() );
@@ -108,5 +84,4 @@ if( !empty( $_REQUEST['version_check'] ) ) {
 
 // Display the template
 $gBitSystem->display( 'bitpackage:kernel/admin.tpl' );
-
 ?>

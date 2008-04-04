daa2f40c31f2cda7c9d6d17b2d34399bfbfba957
jetskijoe <jetskijoe@users.sourceforge.net>
2008-04-04 20:01:22 +0000

    Fixes a bug if you have no Adsense then the mod will not show

--- modules/mod_adsense.php
+++ modules/mod_adsense.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_kernel/modules/mod_adsense.php,v 1.1 2006/12/02 13:45:36 nickpalmer Exp $
+ * $Header: /cvsroot/bitweaver/_bit_kernel/modules/mod_adsense.php,v 1.2 2008/04/04 20:01:22 jetskijoe Exp $
  *
  * Copyright ( c ) 2004 bitweaver.org
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -13,8 +13,6 @@
 if( !empty( $module_params ) ) {
 	$gBitSmarty->assign( 'modParams', $module_params );
 }
-
-$gBitSmarty->assign('modParams', $module_params);
 
 $gBitSmarty->assign('adSenseActive', !empty($module_params['client']) &&
 	$gBitSystem->isFeatureActive( 'liberty_plugin_status_dataadsense' ));

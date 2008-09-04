2ca768d7928d2f757795ece7c61c79ed4e11b1aa
laetzer <laetzer@users.sourceforge.net>
2008-09-04 18:34:37 +0000

    changed words around to avoid class="tab"" tripple q-marks

--- smarty_bit/block.jstab.php
+++ smarty_bit/block.jstab.php
@@ -13,15 +13,21 @@
  * Input:
  * Abstract:	Used to enclose a set of tabs
  */
+ 
 function smarty_block_jstab( $pParams, $pContent, &$gBitSmarty ) {
+
 	// if this is modified, please adjust the preg_match_all() pattern in block.jstabs.php
-	$ret  = '<div class="tabpage'.( isset( $pParams['class'] ) ? ' '.$pParams['class'] : '' ).'">';
-	$ret .= '<h4 class="tab'.( isset( $pParams['class'] ) ? ' '.$pParams['class'] : '' ).'"'.
-				 ( isset( $pParams['onclick'] ) ? ' onclick="'.$pParams['onclick'] : '' ).'">'.
-				 tra( isset( $pParams['title'] ) ? $pParams['title'] : 'No Title' ).
-			'</h4>';
+	$pClass = isset( $pParams['class'] ) ? ' '.$pParams['class'] : '';
+
+	$tClass	= isset( $pParams['class'] ) ? ' '.$pParams['class'] : '';
+	$tClick	= isset( $pParams['onclick'] ) ? ' onclick="'.$pParams['onclick'] : '';
+	$tTitle	= tra( isset( $pParams['title'] ) ? $pParams['title'] : 'No Title' );
+
+	$ret  = '<div class="tabpage' . $pClass . '">';
+	$ret .= '<h4 class="tab' . $tClass	. '"' . $tClick . '>' . $tTitle . '</h4>';
 	$ret .= $pContent;
 	$ret .= '</div>';
+
 	return $ret;
 }
 ?>

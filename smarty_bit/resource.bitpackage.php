91f559da0b7bb81955cd96ea2553bf3344ed733a
Max Kremmel <xing@synapse.plus.com>
2008-09-15 10:24:56 +0000

    fix resource check that tpls can be stored in subdirectories within templates/ - it's now possible to call the tpl: <pkg>/templates/foo/bar.tpl using   bitpackage:pkg/foo/bar.tpl - overrides work as before but tpls need to be in same subdir.   e.g.: themes/templates/<style>/<pkg>/foo/bar.tpl - vastly simplify crazy code and optimise it somewhat - especially if   $gNoForceStyle is set

--- smarty_bit/resource.bitpackage.php
+++ smarty_bit/resource.bitpackage.php
@@ -15,59 +15,28 @@
  * -------------------------------------------------------------
  */
 function smarty_resource_bitpackage_source( $pTplName, &$pTplSource, &$gBitSmarty ) {
-	global $gBitSystem, $gNoForceStyle;
-
 	$resources = smarty_get_bitweaver_resources( $pTplName );
-	extract( $resources );
-
-	if ( empty( $gNoForceStyle ) && file_exists( $force ) ) {
-		$pTplSource = file_get_contents($force);
-		return TRUE;
-	} elseif ( empty( $gNoForceStyle ) && file_exists( $force_simple ) ) {
-		$pTplSource = file_get_contents($force_simple);
-		return TRUE;
-	} elseif ( file_exists( $override ) ) {
-		$pTplSource = file_get_contents($override);
-		return TRUE;
-	} elseif ( file_exists( $override_simple ) ) {
-		$pTplSource = file_get_contents($override_simple);
-		return TRUE;
-	} elseif ( file_exists( $package_template ) ) {
-		$pTplSource = file_get_contents($package_template);
-		return TRUE;
-	} else {
-		$pTplSource = "<p>MISSING TEMPLATE:<br/> <b>p_resource_type:</b> $p_resource_type<br/><b>p_resource_name:</b> $p_resource_name<br/><b>p_template_source:</b> $p_template_source<br/><b>p_template_timestamp:</b> $p_template_timestamp<br/><b>p_smarty_obj:</b> $p_smarty_obj <br /><b>override_template:</b> $override_template<br/><b>package_template:</b>$package_template<br/><b>TIKI Package Path:</b>".BIT_ROOT_PATH;
-		return TRUE;
+	foreach( $resources as $resource ) {
+		if( file_exists( $resource )) {
+			$pTplSource = file_get_contents( $resource );
+			return TRUE;
+		}
 	}
 
+	vd( "Missing template:" );
+	vd( $resources );
 	return FALSE;
 }
 
 // the PHP sibling file needs to be included in modules_inc before this fetch so caching works properly
 function smarty_resource_bitpackage_timestamp( $pTplName, &$pTplTimestamp, &$gBitSmarty ) {
-	global $gBitSystem, $gNoForceStyle;
-
-	$resources = smarty_get_bitweaver_resources( $pTplName );
-	extract( $resources );
-
-	if ( empty( $gNoForceStyle ) && file_exists( $force ) ) {
-		$pTplTimestamp = filemtime( $force );
-		return TRUE;
-	} elseif ( empty( $gNoForceStyle ) && file_exists( $force_simple ) ) {
-		$pTplTimestamp = filemtime( $force_simple );
-		return TRUE;
-	} elseif ( file_exists( $override ) ) {
-		$pTplTimestamp = filemtime( $override );
-		return TRUE;
-	} elseif ( file_exists( $override_simple ) ) {
-		$pTplTimestamp = filemtime( $override_simple );
-		return TRUE;
-	} elseif ( file_exists( $package_template ) ) {
-		$pTplTimestamp = filemtime( $package_template );
-		return TRUE;
-	} else {
-		return FALSE;
+	foreach( smarty_get_bitweaver_resources( $pTplName ) as $resource ) {
+		if( file_exists( $resource )) {
+			$pTplTimestamp = filemtime( $resource );
+			return TRUE;
+		}
 	}
+	return FALSE;
 }
 
 function smarty_resource_bitpackage_secure( $pTplName, &$gBitSmarty ) {
@@ -80,32 +49,32 @@ function smarty_resource_bitpackage_trusted( $pTplName, &$gBitSmarty ) {
 }
 
 function smarty_get_bitweaver_resources( $pTplName ) {
-	global $gBitSystem, $gBitThemes;
+	global $gBitThemes, $gNoForceStyle;
 
 	$path = explode( '/', $pTplName );
 	$package = array_shift( $path );
-	$subdir = '';
 	$template = array_pop( $path );
+	$subdir = '';
 	foreach( $path as $p ) {
 		$subdir .= $p.'/';
 	}
-	// if it's a module, we need to look in the correct path
-	$subdir .= preg_match( '/mod_/', $template ) ? 'modules' : 'templates';
 
-	// look in themes/force/
-	$ret['force']            = THEMES_PKG_PATH."/force/$package/$template";
-	$ret['force_simple']     = THEMES_PKG_PATH."/force/$template";
+	if( empty( $gNoForceStyle )) {
+		// look in themes/force/
+		$ret['force']        = THEMES_PKG_PATH."force/$package/$subdir$template";
+		$ret['force_simple'] = THEMES_PKG_PATH."force/$subdir$template";
+	}
 
 	// look in themes/style/<stylename>/
-	$ret['override']         = $gBitThemes->getStylePath()."/$package/$template";
-	$ret['override_simple']  = $gBitThemes->getStylePath().$template;
+	$ret['override']        = $gBitThemes->getStylePath()."$package/$subdir$template";
+	$ret['override_simple'] = $gBitThemes->getStylePath().$subdir.$template;
+
+	// if it's a module, we need to look in the correct place
+	$subdir = ( preg_match( '/\bmod_/', $template ) ? 'modules' : 'templates' )."/".$subdir;
 
 	// look for default package template
-	// This needs to use the _PKG_PATH constant since the temp dir isn't defined in mPackages
-	$ret['package_template'] = preg_replace( "!/+!", "/", constant( strtoupper( $package ).'_PKG_PATH' )."/$subdir/$template" );
-	//$ret['package_template'] = BIT_ROOT_PATH.$gBitSystem->mPackages[strtolower( $package )]['dir']."/$subdir/$template";
+	$ret['package_template'] = preg_replace( "!/+!", "/", constant( strtoupper( $package ).'_PKG_PATH' )."/$subdir$template" );
 
-	//print "<br/>Time ($package $template)(FORCE: $force_simple<br/>OVERRIDE: $override $override_simple $package_template)<br/>";
 	return $ret;
 }
 ?>

e28f3416da07b9e94e87a39ce6b46e794afa5361
Max Kremmel <xing@synapse.plus.com>
2008-09-15 20:44:10 +0000

    fix resource path to files found in temp/

--- smarty_bit/resource.bitpackage.php
+++ smarty_bit/resource.bitpackage.php
@@ -59,21 +59,29 @@ function smarty_get_bitweaver_resources( $pTplName ) {
 		$subdir .= $p.'/';
 	}
 
-	if( empty( $gNoForceStyle )) {
-		// look in themes/force/
-		$ret['force']        = THEMES_PKG_PATH."force/$package/$subdir$template";
-		$ret['force_simple'] = THEMES_PKG_PATH."force/$subdir$template";
+	// files found in temp are special - these are stored in temp/<pkg>/(templates|modules)/<template.tpl>
+	if( $package == 'temp' ) {
+		// if it's a module, we need to look in the correct place
+		$subdir .= ( preg_match( '/\bmod_/', $template ) ? 'modules' : 'templates' );
+		// we can't override these templates - they only exist in temp
+		$ret['package_template'] = constant( strtoupper( $package ).'_PKG_PATH' )."$subdir/$template";
+	} else {
+		if( empty( $gNoForceStyle )) {
+			// look in themes/force/
+			$ret['force']        = THEMES_PKG_PATH."force/$package/$subdir$template";
+			$ret['force_simple'] = THEMES_PKG_PATH."force/$subdir$template";
+		}
+
+		// look in themes/style/<stylename>/
+		$ret['override']        = $gBitThemes->getStylePath()."$package/$subdir$template";
+		$ret['override_simple'] = $gBitThemes->getStylePath().$subdir.$template;
+
+		// if it's a module, we need to look in the correct place
+		$subdir = ( preg_match( '/\bmod_/', $template ) ? 'modules' : 'templates' )."/".$subdir;
+
+		// look for default package template
+		$ret['package_template'] = constant( strtoupper( $package ).'_PKG_PATH' )."/$subdir$template";
 	}
-
-	// look in themes/style/<stylename>/
-	$ret['override']        = $gBitThemes->getStylePath()."$package/$subdir$template";
-	$ret['override_simple'] = $gBitThemes->getStylePath().$subdir.$template;
-
-	// if it's a module, we need to look in the correct place
-	$subdir = ( preg_match( '/\bmod_/', $template ) ? 'modules' : 'templates' )."/".$subdir;
-
-	// look for default package template
-	$ret['package_template'] = preg_replace( "!/+!", "/", constant( strtoupper( $package ).'_PKG_PATH' )."/$subdir$template" );
 
 	return $ret;
 }

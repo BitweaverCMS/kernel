697824c107532bba2483d5ae565c61e34f4cf8f2
Max Kremmel <xing@synapse.plus.com>
2008-10-17 13:33:57 +0000

    cleanup

--- error.php
+++ error.php
@@ -1,23 +1,17 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_kernel/error.php,v 1.4 2008/06/25 22:21:11 spiderr Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_kernel/error.php,v 1.5 2008/10/17 13:33:57 squareing Exp $
  * @package kernel
  * @subpackage functions
  */
-
-// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
 /**
  * required setup
  */
 require_once( '../bit_setup_inc.php' );
 
-
-
 // Display the template
-$gBitSmarty->assign('msg', strip_tags($_REQUEST["error"]));
-$gBitSystem->display( 'error.tpl' , NULL, array( 'display_mode' => 'display' ));
+$gBitSmarty->assign( 'msg', strip_tags( $_REQUEST["error"] ));
+$gBitSystem->display( 'error.tpl' , tra( 'Error' ), array( 'display_mode' => 'display' ));
 
 ?>

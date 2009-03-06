f9a96d86a6466c304f73c2ca3d2b84301cd35860
Max Kremmel <xing@synapse.plus.com>
2009-03-06 16:50:14 +0000

    update cvs version

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_kernel/config_defaults_inc.php,v 1.44 2009/01/23 16:27:53 bitweaver Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_kernel/config_defaults_inc.php,v 1.45 2009/03/06 16:50:14 squareing Exp $
  * @package kernel
  * @subpackage functions
  */
@@ -131,8 +131,8 @@ if( !defined( 'STORAGE_HOST_URI' ) ) {
 // if this version of bitweaver needs a visit to the installer, update the number in /bit_setup_inc.php
 define( 'BIT_MAJOR_VERSION',	'2' );
 define( 'BIT_MINOR_VERSION',	'5' );
-define( 'BIT_SUB_VERSION',		'0' );
-define( 'BIT_LEVEL',			'' ); // dev < alpha < beta < RC# < '' < pl
+define( 'BIT_SUB_VERSION',		'1' );
+define( 'BIT_LEVEL',			'beta' ); // dev < alpha < beta < RC# < '' < pl
 
 // When updating to certain versions of bitweaver, we need to force a visit to the installer to fix certain stuff in the database.
 // Enter the minimum version number here in the format: '2.1.0-beta'

3d1e694697c92cb4a664647efd46b491758cd505
Lester Caine <lester@lsces.co.uk>
2009-03-31 06:30:03 +0000

    phpdoc update

--- requirements_graph.php
+++ requirements_graph.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_kernel/requirements_graph.php,v 1.2 2009/03/31 06:30:03 lsces Exp $
+ * @package kernel
+ * @subpackage functions
+ */
+
+/**
+ * Setup
+ */
 require_once( '../bit_setup_inc.php' );
 $gBitSystem->verifyPermission( 'p_admin' );
 global $gBitInstaller;

6776e064ac0c81d86709b050a070cc28d473a3d9
wjames5 <will@tekimaki.com>
2009-11-19 19:25:21 +0000

    make sure module has access to config params

--- modules/mod_time.php
+++ modules/mod_time.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_kernel/modules/mod_time.php,v 1.6 2009/10/01 14:17:01 wjames5 Exp $
+ * $Header: /cvsroot/bitweaver/_bit_kernel/modules/mod_time.php,v 1.7 2009/11/19 19:25:21 wjames5 Exp $
  *
  * Copyright ( c ) 2004 bitweaver.org
  * All Rights Reserved. See below for details and a complete list of authors.
@@ -9,6 +9,7 @@
  * @package kernel
  * @subpackage modules
  */
+extract( $moduleParams );
 
 if( !empty( $module_params ) ) {
 	$gBitSmarty->assign( 'modParams', $module_params );

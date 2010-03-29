27e45d43c36c5801ad8d27d7b817bac8e0adf77b
wjames5 <will@tekimaki.com>
2010-03-29 16:57:28 +0000

    fully respect admin setting to disabled bithelp -- if kernel intralink should not be treated link this then it really shouldnt use bithelp or should have a setting to disable

--- smarty_bit/function.bithelp.php
+++ smarty_bit/function.bithelp.php
@@ -15,12 +15,13 @@ require_once('function.biticon.php');
 function smarty_function_bithelp($params, &$gBitSmarty) {
 	global $gBitSystem, $gBitUser;
 	$outstr = "";
-	if($gBitUser->hasPermission( 'p_admin' )){
-		$outstr .= "<a href=\"".KERNEL_PKG_URL."admin/index.php\">".smarty_function_biticon(array('ipackage'=>'icons', 'iname'=>'preferences-system', 'iexplain'=>'Administration Menu'),$gBitSmarty)."</a> ";
-	}
-	if($gBitSystem->isFeatureActive('site_online_help') && ( $helpInfo = $gBitSmarty->get_template_vars('TikiHelpInfo') ) ) {
-		$outstr .= "<a href=\"".$helpInfo["URL"]."\" >".smarty_function_biticon(array('ipackage'=>'icons', 'iname'=>'help-browser', 'iexplain'=>(empty($helpInfo["Desc"])?"help":$helpInfo["Desc"])),$gBitSmarty)."</a>";
+	if( $gBitSystem->isFeatureActive('site_online_help') ){
+		if($gBitUser->hasPermission( 'p_admin' )){
+			$outstr .= "<a href=\"".KERNEL_PKG_URL."admin/index.php\">".smarty_function_biticon(array('ipackage'=>'icons', 'iname'=>'preferences-system', 'iexplain'=>'Administration Menu'),$gBitSmarty)."</a> ";
+		}
+		if( $helpInfo = $gBitSmarty->get_template_vars('TikiHelpInfo') ) {
+			$outstr .= "<a href=\"".$helpInfo["URL"]."\" >".smarty_function_biticon(array('ipackage'=>'icons', 'iname'=>'help-browser', 'iexplain'=>(empty($helpInfo["Desc"])?"help":$helpInfo["Desc"])),$gBitSmarty)."</a>";
+		}
 	}
 	return $outstr;
 }
-?>

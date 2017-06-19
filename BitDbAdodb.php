17904263117ca3b288212337dccd363c784bfb4f
spider@app2 <spiderr@bitweaver.org>
2017-06-19 17:52:27 -0400

    default ADODB cache in sys_get_temp_dir

--- BitDbAdodb.php
+++ BitDbAdodb.php
@@ -20,7 +20,7 @@
  */
 global $ADODB_CACHE_DIR;
 if( empty( $ADODB_CACHE_DIR )) {
-	$ADODB_CACHE_DIR = get_temp_dir().'/adodb/'.$_SERVER['HTTP_HOST'].'/';
+	$ADODB_CACHE_DIR = sys_get_temp_dir().'/php/adodb/'.$_SERVER['HTTP_HOST'].'/';
 }
 mkdir_p( $ADODB_CACHE_DIR );
 

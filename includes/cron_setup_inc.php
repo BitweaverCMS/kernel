89910cf2c147f8be9eac5ff620bcb4041c10e592
spiderr <spiderr@bitweaver.org>
2022-12-15 14:05:58 -0500

    add cron_setup_inc

---
+++ includes/cron_setup_inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$_SERVER['SERVER_NAME'] = gethostname();
+$_SERVER['SCRIPT_URL'] = __FILE__;
+$_SERVER['REMOTE_ADDR'] = 'cron';
+$_SERVER['SERVER_ADDR'] = '127.0.0.1';
+$_SERVER['REQUEST_URI'] = 'cron';
+$_SERVER['REMOTE_ADDR'] = 'cron';
+$_SERVER['HTTP_HOST'] = '';
+
+
